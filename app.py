e1eaf04b2c04044a1f3fca4b985d4ca9f5d2a311
DaYeong96 <102666062+DaYeong96@users.noreply.github.com>
2022-06-09 21:33:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ ohe_station = OneHotEncoder(sparse=False)
 
 
 data_cat2 = ohe_station.fit_transform(new_x_df[['station']])
-#data_concat = pd.concat([new_x_df.drop(columns=['station']),pd.DataFrame(data_cat2, columns=['station_' + str(col) for col in ohe_station.categories_[0]])], axis=1)
+data_concat = pd.concat([new_x_df.drop(columns=['station']),pd.DataFrame(data_cat2, columns=['station_' + str(col) for col in ohe_station.categories_[0]])], axis=1)
 
 
 
